61feb7d5caf61bfb0081e5141edeab6964001877
vishnuchandrappan <cvishnu70@gmail.com>
2020-01-21 12:50:10 +0530

    added bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 export default function Login() {
     return (
-      <div className="login-page">
+      <div className="login-page container-fluid">
         <h1 className="page-title">Login</h1>
         <form>
           <div className="form-group">
             <label htmlFor="username">Username / Email ID</label>
-            <input type="text" name="username" id="username" />
+            <input className="form-control" type="text" name="username" id="username" />
           </div>
           <div className="form-group">
             <label htmlFor="username">Password</label>
-            <input type="password" name="password" id="password" />
+            <input className="form-control" type="password" name="password" id="password" />
           </div>
           <div className="btn-container">
-            <input type="submit" value="Login" />
+            <input className="btn btn-dark" type="submit" value="Login" />
           </div>
           <div className="form-group">
             <span>Forgot Password ? Reset here</span>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,23 +3,23 @@ import React from 'react';
 export default function Signup() {
     return (
       <div>
-        <div className="login-page">
+        <div className="login-page container-fluid">
           <h1 className="page-title">Signup</h1>
           <form>
             <div className="form-group">
               <label htmlFor="username">Email ID</label>
-              <input type="text" name="username" id="username" />
+              <input className="form-control" type="text" name="username" id="username" />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" />
+              <input className="form-control" type="password" name="password" id="password" />
             </div>
             <div className="form-group">
               <label htmlFor="password_confirmation">Confirm Password</label>
-              <input type="password" name="password_confirmation" id="password_confirmation" />
+              <input className="form-control" type="password" name="password_confirmation" id="password_confirmation" />
             </div>
             <div className="btn-container">
-              <input type="submit" value="Signup" />
+              <input className="btn btn-dark" type="submit" value="Signup" />
             </div>
             <div className="errors">
 
