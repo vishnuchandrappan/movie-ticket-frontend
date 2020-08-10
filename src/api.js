89081bc1c1bb0a6f3a6dc91ed1e9177a93a704c9
vishnuchandrappan <cvishnu70@gmail.com>
2020-08-10 14:11:38 +0530

    final

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: `http://localhost:8000`
-  baseURL: `https://gentle-bastion-82843.herokuapp.com/`
+  baseURL: `http://localhost:8000`
+  // baseURL: `https://gentle-bastion-82843.herokuapp.com/`
 });
